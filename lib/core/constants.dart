0d6b5bfa382f1190cd678ff79ab8bdbab0a7ff7c
GUINO225 <ton-email@exemple.com>
2026-04-30 21:16:55 +0000

    fusion initiale

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -6,7 +6,7 @@ const String kAdminWhatsApp = '2250749931142';
 /// pour faciliter la personnalisation par écran.
 const screenTextConfig = {
   'landing': {
-    'tagline': 'Réservez votre beauté\nen quelque clics',
+    'tagline': '',
     'primaryCta': 'COMMENCER',
     'secondaryCta': 'SE CONNECTER',
     'commentLabel': 'Commentaire',
@@ -38,25 +38,25 @@ const screenStyleConfig = {
     'topSpacerFlex': 6, // Espace flexible avant le bloc principal.
     'middleSpacerFlex': 3, // Espace flexible entre logo et actions.
     'bottomSpacerFlex': 1, // Espace flexible sous les boutons.
-    'logoWidth': 240.0, // Largeur fixe du logo.
-    'taglineFontSize': 24.0, // Taille du texte d'accroche.
+    'logoWidth': 300.0, // Largeur fixe du logo.
+    'taglineFontSize': 35.0, // Taille du texte d'accroche.
     'taglineLineHeight': 1.34, // Hauteur de ligne du texte d'accroche.
     'taglineWeight': FontWeight.w400, // Graisse du texte d'accroche.
     'ctaVerticalGap': 40.0, // Espace vertical avant le bouton principal.
     'buttonHeight': 72.0, // Hauteur des boutons d'action.
-    'buttonRadius': 16.0, // Rayon des coins des boutons.
-    'primaryCtaFontSize': 22.0, // Taille de police du bouton principal.
+    'buttonRadius': 10.0, // Rayon des coins des boutons.
+    'primaryCtaFontSize': 16.0, // Taille de police du bouton principal.
     'primaryCtaLetterSpacing': 0.5, // Espacement des lettres du bouton principal.
     'primaryCtaWeight': FontWeight.w500, // Graisse du texte du bouton principal.
-    'primaryCtaIconSize': 46.0, // Taille de l'icône dans le bouton principal.
+    'primaryCtaIconSize': 36.0, // Taille de l'icône dans le bouton principal.
     'primaryCtaIconGap': 6.0, // Espace entre le texte et l'icône du bouton principal.
     'betweenButtonsGap': 18.0, // Espace vertical entre les deux boutons.
-    'secondaryCtaFontSize': 20.0, // Taille de police du bouton secondaire.
+    'secondaryCtaFontSize': 16.0, // Taille de police du bouton secondaire.
     'secondaryCtaLetterSpacing': 1.0, // Espacement des lettres du bouton secondaire.
     'secondaryCtaWeight': FontWeight.w500, // Graisse du texte du bouton secondaire.
     'secondaryBorderOpacity': 0.68, // Opacité de la bordure du bouton secondaire.
     'secondaryBorderWidth': 2.0, // Épaisseur de la bordure du bouton secondaire.
-    'indicatorSize': 28.0, // Taille des indicateurs (points/pastilles).
+    'indicatorSize': 15.0, // Taille des indicateurs (points/pastilles).
     'indicatorGap': 18.0, // Espace entre les indicateurs.
     'indicatorBorderWidth': 2.2, // Épaisseur de bordure des indicateurs.
   },
